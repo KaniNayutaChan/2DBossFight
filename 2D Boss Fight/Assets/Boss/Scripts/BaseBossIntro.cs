2e3c725ed84e08b5eff73b9621d582fc46117d4c
JoshuaTanAIE <s200098@students.aie.edu.au>
2021-06-03 11:33:12 +0930

    intro

---
+++ 2D Boss Fight/Assets/Boss/Scripts/BaseBossIntro.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseBossIntro : BaseBoss
+{
+    [Tooltip("The distance from the boss to the player which wakes the boss up.")]
+    public float visionRange;
+
+    [Tooltip("The amount of time elapsed until the boss goes to its idle state.")]
+    public float timeTillIdle;
+
+    bool isAwake;
+
+
+    public override void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        base.OnStateEnter(animator, stateInfo, layerIndex);
+
+        isAwake = false;
+    }
+
+    public override void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        base.OnStateUpdate(animator, stateInfo, layerIndex);
+
+        if(Vector2.Distance(enemyPos.position, playerPos.position) < visionRange)
+        {
+            isAwake = true;
+        }
+
+        if(isAwake)
+        {
+            if(timeTillIdle > 0)
+            {
+                timeTillIdle -= Time.deltaTime;
+            }
+            else
+            {
+                animator.Play("Idle");
+            }
+        }
+    }
+}
---
+++ 2D Boss Fight/Assets/Boss/Scripts/BossHealth.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+public class BossHealth : MonoBehaviour
+{
+    public float maxHealth;
+    [HideInInspector] public float currentHealth;
+    Animator animator;
+    bool isDead;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        currentHealth = maxHealth;
+        isDead = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(currentHealth < 0 && !isDead)
+        {
+            isDead = true;
+            Die();
+        }
+    }
+
+    void Die()
+    {
+        animator.Play("Death");
+    }
+}
