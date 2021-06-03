0b40e697a3c6cc707a582b907330a96e2f99c8d0
JoshuaTanAIE <s200098@students.aie.edu.au>
2021-06-03 15:49:00 +0930

    added in spawnpos

--- 2D Boss Fight/Assets/Boss/Scripts/BaseBossAttack.cs
+++ 2D Boss Fight/Assets/Boss/Scripts/BaseBossAttack.cs
@@ -6,12 +6,6 @@ public class BaseBossAttack : BaseBoss
 {
     public float startTimeTillNextAnimation;
     protected Vector2 spawnPos;
-    public int numberOfAttacks;
-    int attackCounter;
-    public float startTimeTillAttack;
-    public float startTimeBetweenAttacks;
-    protected float timeTillAttack;
-    public GameObject attackPrefab;
 
     public SpawnTimingType spawnTimingType;
     public enum SpawnTimingType
@@ -46,6 +40,13 @@ public class BaseBossAttack : BaseBoss
         InIncrements,
         InCircle
     }
+
+    public GameObject attackPrefab;
+    public int numberOfAttacks;
+    int attackCounter;
+    public float startTimeTillAttack;
+    public float startTimeBetweenAttacks;
+    protected float timeTillAttack;
 
     public override void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
@@ -95,6 +96,56 @@ public class BaseBossAttack : BaseBoss
         {
             attackCounter++;
             timeTillAttack = startTimeBetweenAttacks;
+
+            switch (spawnType)
+            {
+                case SpawnType.RelativeToEnemy:
+                    if(IsFacingRight())
+                    {
+                        spawnPos.Set(enemyPos.position.x + spawnRelativeToEnemyX, enemyPos.position.y + spawnRelativeToEnemyY);
+                    }
+                    else
+                    {
+                        spawnPos.Set(enemyPos.position.x - spawnRelativeToEnemyX, enemyPos.position.y + spawnRelativeToEnemyY);
+                    }
+                    break;
+
+                case SpawnType.RelativeToPlayer:
+                    if (IsFacingRight())
+                    {
+                        spawnPos.Set(playerPos.position.x + spawnRelativeToPlayerX, playerPos.position.y + spawnRelativeToPlayerY);
+                    }
+                    else
+                    {
+                        spawnPos.Set(playerPos.position.x - spawnRelativeToPlayerX, playerPos.position.y + spawnRelativeToPlayerY);
+                    }
+                    break;
+
+                case SpawnType.RandomPosition:
+                    spawnPos.Set(Random.Range(spawnMinRandomX, spawnMaxRandomX), Random.Range(spawnMinRandomY, spawnMaxRandomY));
+                    break;
+
+                case SpawnType.Set:
+                    spawnPos.Set(spawnSetX, spawnSetY);
+                    break;
+
+                case SpawnType.InIncrements:
+                    if (IsFacingRight())
+                    {
+                        spawnPos.Set(spawnStartIncrementX - (attackCounter * spawnIncrementalDistance), spawnStartIncrementY);
+                    }
+                    else
+                    {
+                        spawnPos.Set(-spawnStartIncrementX + (attackCounter * spawnIncrementalDistance), spawnStartIncrementY);
+                    }
+                    break;
+
+                case SpawnType.InCircle:
+                    spawnPos.Set(playerPos.position.x + (spawnCircleAmplitude * Mathf.Cos(2 * Mathf.PI * attackCounter / numberOfAttacks)), playerPos.position.y + (spawnCircleAmplitude * Mathf.Sin(2 * Mathf.PI * attackCounter / numberOfAttacks)));
+                    break;
+            }
+
+
             Instantiate(attackPrefab, spawnPos, enemyPos.rotation);
         }
     }
